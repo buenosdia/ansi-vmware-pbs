3eab26355a60ea6833d8837038e9592daa93ca50
git <git@geosolinc.net>
2024-09-18 15:54:09 -0400

    fdafedf

--- roles/files/setup.ps1
+++ roles/files/setup.ps1
@@ -6,8 +6,8 @@ $null = gpupdate /target:computer /force /wait:0
 #Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
 $null = Set-Service -Name "WinRM" -StartupType Automatic -Confirm:$false
 $null = Start-Service -Name "WinRM" -Confirm:$false
-Get-Certificate -Template "GSIWinRMSSL" -Url ldap:
-Get-Certificate -Template "GSI Rdp" -Url ldap:
+Get-Certificate -Template "GSIWinRMSSL" -Url ldap: -CertStoreLocation Cert:\LocalMachine\My
+Get-Certificate -Template "GSI Rdp" -Url ldap: -CertStoreLocation Cert:\LocalMachine\My
 $ruleDisplayName = 'Windows Remote Management (HTTPS-In)'
 
 if (-not (Get-NetFirewallRule -DisplayName $ruleDisplayName -ErrorAction Ignore)) {
